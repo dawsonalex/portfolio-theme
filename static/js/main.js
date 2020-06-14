e76f16a8885f33b8fb98aba9d6fce483c7966fe2
Alex <alexander_dawson@ymail.com>
2020-06-14 13:00:43 +0100

    Nav menu changes
    - show / hide nav menu.
    - SIze nav menu.

--- static/js/main.js
+++ static/js/main.js
@@ -3,3 +3,28 @@
  * 
  * @author Alex Dawson
  */
+
+(function() {
+	let menu = document.getElementById('main-menu');
+	let openMenuButton = document.getElementById('menu-open');
+	let closeMenuButton = document.getElementById('menu-close');
+
+	/**
+	 * Display the navigation menu overlay.
+	 */
+	function showMenu() {
+		menu.classList.add('d-flex');
+		menu.classList.remove('d-none');
+	}
+
+	/**
+	 * Hide the navigation menu overlay.
+	 */
+	function hideMenu() {
+		menu.classList.add('d-none');
+		menu.classList.remove('d-flex');
+	}
+
+	openMenuButton.addEventListener('click', showMenu);
+	closeMenuButton.addEventListener('click', hideMenu);
+}());
