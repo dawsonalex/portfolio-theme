93048e1c2c42d6635fb118c546b69d56f42320d0
Alexander Dawson <alexander_dawson@ymail.com>
2020-06-16 16:12:43 +0100

    Nav menu changes
    - Add menu slide in animation.

--- static/js/main.js
+++ static/js/main.js
@@ -6,23 +6,30 @@
 
 (function() {
 	let menu = document.getElementById('main-menu');
+	// main menu doesn't exist if header menu isn't set.
+	if (!menu) {
+		return;
+	}
 	let openMenuButton = document.getElementById('menu-open');
 	let closeMenuButton = document.getElementById('menu-close');
+	let contentOverlay = document.querySelector('.header-nav-overlay');
 
 	/**
 	 * Display the navigation menu overlay.
 	 */
 	function showMenu() {
-		menu.classList.add('d-flex');
-		menu.classList.remove('d-none');
+		menu.classList.remove('header-nav-closed');
+		document.body.classList.add('noscroll');
+		contentOverlay.classList.add('header-nav-overlay-visible');		
 	}
 
 	/**
 	 * Hide the navigation menu overlay.
 	 */
 	function hideMenu() {
-		menu.classList.add('d-none');
-		menu.classList.remove('d-flex');
+		menu.classList.add('header-nav-closed');
+		document.body.classList.remove('noscroll');
+		contentOverlay.classList.remove('header-nav-overlay-visible');	
 	}
 
 	openMenuButton.addEventListener('click', showMenu);
